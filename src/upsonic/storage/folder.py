7f33929236de5ff6f95d5aaf5605725c38d08354
Onur ULUSOY <atadogan06@gmail.com>
2025-03-27 13:16:34 +0300

    feat: Add environment variable support for BASE_PATH configuration (#311)

--- src/upsonic/storage/folder.py
+++ src/upsonic/storage/folder.py
@@ -1,4 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Define a variable to store the current file's directory path
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+if os.getenv("USE_WORKDIR", "false").lower() == "true":
+    BASE_PATH = os.path.dirname(os.getcwd())
+else:
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
