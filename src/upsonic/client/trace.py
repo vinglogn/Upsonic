f63d0a008dfb68f012a46afbf83519bd32bc0c66
Onur ULUSOY <atadogan06@gmail.com>
2025-04-03 15:44:51 +0300

    feat: Load environment variables for Sentry configuration and set environment in telemetry

--- src/upsonic/client/trace.py
+++ src/upsonic/client/trace.py
@@ -1,14 +1,17 @@
 import sentry_sdk as sentry_sdk_
 from sentry_sdk.transport import Transport
+from dotenv import load_dotenv
 
 from ..get_version import get_library_version
 from ..system_id import get_system_id
 
 
-
 import os
 
+load_dotenv()
+
 the_dsn = os.getenv("UPSONIC_TELEMETRY", "https://7023ec3e0699da14a8013478e50b9142@o4508336623583232.ingest.us.sentry.io/4508607159599104")
+the_environment = os.getenv("UPSONIC_ENVIRONMENT", "production")
 
 if the_dsn.lower() == "false":
     the_dsn = ""
@@ -18,7 +21,7 @@ sentry_sdk_.init(
     traces_sample_rate=1.0,
     release=f"upsonic@{get_library_version()}",
     server_name="upsonic_client",
-
+    environment=the_environment,
 )
 
 sentry_sdk_.set_user({"id": get_system_id()})
